327905941f3d6cc9f805d19a4d3f25ec13d1fdc6
v1rtl <hi@v1rtl.site>
2023-12-15 16:42:56 +0200

    get current bus stop

--- web/www/main.go
+++ web/www/main.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"math"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	_ "github.com/lib/pq"
@@ -20,11 +22,49 @@ type Template struct {
 }
 
 type Trip struct {
+	RouteID        string `json:"route_id"`
 	RouteShortName string `json:"route_short_name"`
 	TripID         string `json:"trip_id"`
 	TripLongName   string `json:"trip_long_name"`
 	ArrivalTime    string `json:"arrival_time"`
 	DepartureTime  string `json:"departure_time"`
+	Lat            string `json:"stop_lat"`
+	Lon            string `json:"stop_lon"`
+}
+
+type Stop struct {
+	Name string  `json:"name"`
+	ID   string  `json:"id"`
+	Lat  float64 `json:"lat"`
+	Lon  float64 `json:"lon"`
+}
+
+func haversine(lat1, lon1, lat2, lon2 float64) float64 {
+	const R = 6371 // Radius of the Earth in km
+	dLat := (lat2 - lat1) * math.Pi / 180.0
+	dLon := (lon2 - lon1) * math.Pi / 180.0
+
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(lat1*math.Pi/180.0)*math.Cos(lat2*math.Pi/180.0)*
+			math.Sin(dLon/2)*math.Sin(dLon/2)
+
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	return R * c // Distance in km
+}
+
+func findNearestStop(stops []Stop, userLat, userLon float64) Stop {
+	var nearest Stop
+	minDistance := math.MaxFloat64
+
+	for _, stop := range stops {
+		distance := haversine(userLat, userLon, stop.Lat, stop.Lon)
+		if distance < minDistance {
+			minDistance = distance
+			nearest = stop
+		}
+	}
+
+	return nearest
 }
 
 func main() {
@@ -108,7 +148,7 @@ func main() {
 	http.HandleFunc("/api/buses", func(w http.ResponseWriter, r *http.Request) {
 		stop := r.URL.Query().Get("stop")
 
-		rows, err := db.Query("SELECT DISTINCT ON (r.route_short_name) r.route_short_name, t.trip_id, t.trip_long_name, st.arrival_time, st.departure_time FROM stops s JOIN stop_times st ON s.stop_id = st.stop_id JOIN trips t ON st.trip_id = t.trip_id JOIN routes r ON t.route_id = r.route_id WHERE s.stop_name = $1 ORDER BY r.route_short_name, st.arrival_time", stop)
+		rows, err := db.Query("SELECT DISTINCT ON (r.route_short_name) r.route_id, r.route_short_name, t.trip_id, t.trip_long_name, st.arrival_time, st.departure_time, s.stop_lat, s.stop_lon FROM stops s JOIN stop_times st ON s.stop_id = st.stop_id JOIN trips t ON st.trip_id = t.trip_id JOIN routes r ON t.route_id = r.route_id WHERE s.stop_name = $1 ORDER BY r.route_short_name, st.arrival_time", stop)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Error querying buses: %v", err), http.StatusInternalServerError)
 			return
@@ -119,7 +159,7 @@ func main() {
 
 		for rows.Next() {
 			var trip Trip
-			err := rows.Scan(&trip.RouteShortName, &trip.TripID, &trip.TripLongName, &trip.ArrivalTime, &trip.DepartureTime)
+			err := rows.Scan(&trip.RouteID, &trip.RouteShortName, &trip.TripID, &trip.TripLongName, &trip.ArrivalTime, &trip.DepartureTime, &trip.Lat, &trip.Lon)
 			if err != nil {
 				http.Error(w, fmt.Sprintf("Error scanning row: %v", err), http.StatusInternalServerError)
 				return
@@ -163,5 +203,52 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/api/arrivals", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		routeID := query.Get("route_id")
+		userLatStr := query.Get("user_lat")
+		userLonStr := query.Get("user_lon")
+
+		userLat, err := strconv.ParseFloat(userLatStr, 64)
+		if err != nil {
+			http.Error(w, "Invalid user latitude", http.StatusBadRequest)
+			return
+		}
+
+		userLon, err := strconv.ParseFloat(userLonStr, 64)
+		if err != nil {
+			http.Error(w, "Invalid user longitude", http.StatusBadRequest)
+			return
+		}
+
+		rows, err := db.Query(`
+            SELECT DISTINCT s.stop_name, s.stop_id, s.stop_lat, s.stop_lon
+            FROM stops s
+            JOIN stop_times st ON s.stop_id = st.stop_id
+            JOIN trips t ON st.trip_id = t.trip_id
+            JOIN routes r ON t.route_id = r.route_id
+            WHERE r.route_id = $1`, routeID)
+
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Database query error: %s", err), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var stops []Stop
+		for rows.Next() {
+			var stop Stop
+			if err := rows.Scan(&stop.Name, &stop.ID, &stop.Lat, &stop.Lon); err != nil {
+				http.Error(w, "Error reading stops data", http.StatusInternalServerError)
+				return
+			}
+			stops = append(stops, stop)
+		}
+		nearestStop := findNearestStop(stops, userLat, userLon)
+
+		json.NewEncoder(w).Encode(nearestStop)
+	})
+
+	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
--- web/www/public/buses.js
+++ web/www/public/buses.js
@@ -6,10 +6,41 @@ const json = await res.json()
 
 const grid = document.getElementById('grid')
 
+/**
+ * @type {GeolocationCoordinates}
+ */
+let coords = {}
+
+navigator.geolocation.getCurrentPosition((pos) => {
+  coords = pos.coords
+}, (err) => {
+  console.error(err)
+})
+
 for (const bus of json) {
   const btn = document.createElement('button')
   btn.textContent = `${bus.route_short_name} (${bus.trip_long_name})`
-  btn.classList.add('bg-green-500', 'hover:bg-green-700' ,'text-white' ,'font-bold' ,'py-1' ,'px-2','text-sm' ,'rounded')
+  btn.classList.add(
+    'bg-green-500',
+    'hover:bg-green-700',
+    'text-white',
+    'font-bold',
+    'py-1',
+    'px-2',
+    'text-sm',
+    'rounded',
+  )
+
+  btn.onclick = () => {
+    fetch(
+      `/api/arrivals?user_lat=${coords.latitude}&user_lon=${coords.longitude}&route_id=${bus.route_id}`,
+    )
+      .then((res) => res.json()).then((json) => {
+        console.log(`Closest stop: `, json)
+
+        console.log(bus)
+      })
+  }
 
   grid.appendChild(btn)
 }
